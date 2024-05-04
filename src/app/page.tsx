3ed0e65a7dc1dfb1db51f64651047b926f8d6d20
raxitvaghela <rvaghela@lumosfinserv.com>
2024-05-04 11:58:07 +0530

    Revert "Revert "Created Cart Icon""
    This reverts commit 3bc7fee6e607675e7073a9bf75a7bafbc027d663.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-import HomePage from '@/components/Home/Home';
-import SignIn from '@/components/auth/SignIn';
 import SignUp from '@/components/auth/SignUp';
 
 export default function Home() {
